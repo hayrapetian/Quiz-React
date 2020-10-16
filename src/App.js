3e922a1462f2ca27d2cc59c3db5e3a9c539d7e5a
Hayk Hayrapetian <50294522+hayrapetian@users.noreply.github.com>
2020-10-16 16:13:01 +0400

    Adding buttons and shuffle function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import Questionnaire from './components/Questionnaire'
+
+const API_URL =
+  'https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [questions, setQuestions] = useState([])
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => {
+        setQuestions(data.results)
+      })
+  }, [])
+
+  const handleAnswer = answer => {
+    setCurrentIndex(currentIndex + 1)
+  }
+
+  return questions.length > 0 ? (
+    <div className='container'>
+      <Questionnaire
+        data={questions[currentIndex]}
+        handleAnswer={handleAnswer}
+      />
     </div>
-  );
+  ) : (
+    <>
+      <div class='loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32'></div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Questionnaire.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Questionnaire = ({
+  handleAnswer,
+  data: { question, correct_answer, incorrect_answers },
+}) => {
+  const shuffledAnswers = [correct_answer, ...incorrect_answers].sort(
+    () => Math.random() - 0.5
+  )
+  return (
+    <>
+      <div className='bg-white p-10 rounded-lg shadow-xl text-purple-800'>
+        <h2
+          className='text-2xl'
+          dangerouslySetInnerHTML={{ __html: question }}
+        />
+      </div>
+      <div className='mt-4 flex flex-wrap justify-between'>
+        {shuffledAnswers.map(answer => (
+          <button className='bg-white w-5/12 p-4 text-purple-800 rounded-lg font-semibold mb-4 shadow-lg' onClick={() => handleAnswer(answer) }>
+            {answer}
+          </button>
+        ))}
+      </div>
+    </>
+  )
+}
+export default Questionnaire
